31f741960cb1a0732178c19abb7512dc9a431bc5
KrukowskiJ <jakub.krukowski@student.wat.edu.pl>
2021-12-11 00:24:30 +0100

    add basic sidenavigation add header with company logo

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/new')
+@app.route('/sales')
+@app.route('/shoes')
+@app.route('/accessories')
+@app.route('/lookbook')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/clothes')
+def hello_world1():  # put application's code here
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
