2a5256da09ce7aec659864faf19c1ced6090a9ef
KrukowskiJ <jakub.krukowski@student.wat.edu.pl>
2021-12-11 22:28:41 +0100

    add men/women buttons switcher and first items

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+sex = "Men"
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    global sex
+    if request.method == 'POST':
+        if request.form.get('action1') == 'Men':
+            sex = request.form.get('action1')
+        elif request.form.get('action2') == 'Women':
+            sex = request.form.get('action2')
+    elif request.method == 'GET':
+        return render_template('index.html')
+    return render_template('index.html', sex=sex)
 
-
-@app.route('/')
-@app.route('/new')
-@app.route('/sales')
-@app.route('/shoes')
-@app.route('/accessories')
-@app.route('/lookbook')
-def hello_world():  # put application's code here
-    return render_template('index.html')
-
-@app.route('/clothes')
-def hello_world1():  # put application's code here
-    return render_template('index.html')
-
+@app.route('/<x>', methods=['GET', 'POST'])
+def hello_world(x):  # put application's code here
+    global sex
+    if request.method == 'POST':
+        if request.form.get('action1') == 'Men':
+            sex = request.form.get('action1')
+        elif request.form.get('action2') == 'Women':
+            sex = request.form.get('action2')
+        return render_template('items.html', sex=sex, items=x)
+    else:
+        return render_template('items.html', sex=sex, items=x)
 
 if __name__ == '__main__':
     app.run()
