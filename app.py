d909d981dc1a04f61f323e2c2855a34e53f12242
KrukowskiJ <jakub.krukowski@student.wat.edu.pl>
2021-12-16 00:00:22 +0100

    add first data in json, import sex/category/products from json

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request,  url_for, json
 app = Flask(__name__)
+
+
+
 sex = "Men"
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -16,14 +20,16 @@ def index():
 @app.route('/<x>', methods=['GET', 'POST'])
 def hello_world(x):  # put application's code here
     global sex
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    data = json.load(open(os.path.join(SITE_ROOT, "static", "data\data.json"), "r"))
     if request.method == 'POST':
         if request.form.get('action1') == 'Men':
             sex = request.form.get('action1')
         elif request.form.get('action2') == 'Women':
             sex = request.form.get('action2')
-        return render_template('items.html', sex=sex, items=x)
+        return render_template('items.html', sex=sex, items=x,items_table=data)
     else:
-        return render_template('items.html', sex=sex, items=x)
+        return render_template('items.html', sex=sex, items=x,items_table=data)
 
 if __name__ == '__main__':
     app.run()
