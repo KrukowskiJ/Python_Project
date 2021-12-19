72940e64e54e6011a7e1209b2d75c668291938a3
KrukowskiJ <jakub.krukowski@student.wat.edu.pl>
2021-12-20 00:29:40 +0100

    add first cart ( page / icon ) + fix href links in categories

--- app.py
+++ app.py
@@ -1,35 +1,32 @@
 import os
-from flask import Flask, render_template, request,  url_for, json
+from flask import Flask, render_template, request,  url_for, json,redirect
 app = Flask(__name__)
 
+class Cart:
+  def __init__(self, count, sum,items):
+    self.count = count
+    self.sum = sum
+    self.items = items
 
+@app.route('/')
+def redirect():
+    return redirect("/men", code=302)
 
-sex = "Men"
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    global sex
-    if request.method == 'POST':
-        if request.form.get('action1') == 'Men':
-            sex = request.form.get('action1')
-        elif request.form.get('action2') == 'Women':
-            sex = request.form.get('action2')
-    elif request.method == 'GET':
-        return render_template('index.html')
+@app.route("/<sex>", methods=['GET', 'POST'])
+def start(sex):
     return render_template('index.html', sex=sex)
 
-@app.route('/<x>', methods=['GET', 'POST'])
-def hello_world(x):  # put application's code here
-    global sex
+@app.route('/<sex>/<items>', methods=['GET', 'POST'])
+def itemspage(sex,items):  # put application's code here
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     data = json.load(open(os.path.join(SITE_ROOT, "static", "data\data.json"), "r"))
-    if request.method == 'POST':
-        if request.form.get('action1') == 'Men':
-            sex = request.form.get('action1')
-        elif request.form.get('action2') == 'Women':
-            sex = request.form.get('action2')
-        return render_template('items.html', sex=sex, items=x,items_table=data)
-    else:
-        return render_template('items.html', sex=sex, items=x,items_table=data)
+    return render_template('items.html', sex=sex, items=items,items_table=data)
+
+@app.route('/cart')
+def cart():
+    c1 = Cart(0,0,0)
+    return render_template('cart.html',cart=c1,items=1)
+
 
 if __name__ == '__main__':
     app.run()
