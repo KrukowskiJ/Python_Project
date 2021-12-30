e43ff4725858fb3e8022b39c5abf228129d89ff6
KrukowskiJ <jakub.krukowski.business@gmail.com>
2021-12-30 18:57:12 +0100

    add flask layouts make cart working

--- app.py
+++ app.py
@@ -3,14 +3,25 @@ from flask import Flask, render_template, request,  url_for, json,redirect
 app = Flask(__name__)
 
 class Cart:
-  def __init__(self, count, sum,items):
+  def __init__(self, count, sum, items):
     self.count = count
     self.sum = sum
-    self.items = items
+    self.items = [items]
+
+global_cart = Cart(0,0,'')
+@app.route('/addcart', methods=['POST'])
+def AddCart():
+    product_name =request.form.get('product_name')
+    product_price = int(request.form.get('product_price'))
+
+    global_cart.sum += product_price
+    global_cart.items.append(product_name)
+    global_cart.count += 1
+    return redirect("/cart")
 
 @app.route('/')
-def redirect():
-    return redirect("/men", code=302)
+def redirectx():
+    return redirect("/men")
 
 @app.route("/<sex>", methods=['GET', 'POST'])
 def start(sex):
@@ -24,9 +35,11 @@ def itemspage(sex,items):  # put application's code here
 
 @app.route('/cart')
 def cart():
-    c1 = Cart(0,0,0)
-    return render_template('cart.html',cart=c1,items=1)
+    return render_template('cart.html',cart=global_cart,items=1)
 
+@app.route('/user')
+def carrt():
+    return render_template('cart.html',cart=global_cart,items=1)
 
 if __name__ == '__main__':
     app.run()
